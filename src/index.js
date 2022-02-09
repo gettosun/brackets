068a2867123c7dbfd2f438f08a055d6369f921ac
Eugene Tsaprilov <gettosun@gmail.com>
2022-02-09 15:49:26 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const arr = bracketsConfig.map((pair) => pair.join(''));
+  
+    for (let i = 0; i < arr.length;) {
+      if (str.includes(arr[i])) {
+        str = str.replace(arr[i], '');
+        i = 0;
+    } else { 
+        i++;
+      }
+    }
+    return str.length == 0;
+};
